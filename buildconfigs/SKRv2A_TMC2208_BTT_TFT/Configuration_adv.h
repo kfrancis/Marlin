db0e733cafff6c8f3c7395ff6cb654f55f1b33ea
Kori Francis <kfrancis@clinicalsupportsystems.com>
2021-06-21 12:47:55 -0400

    fix the custom macros

--- buildconfigs/SKRv2A_TMC2208_BTT_TFT/Configuration_adv.h
+++ buildconfigs/SKRv2A_TMC2208_BTT_TFT/Configuration_adv.h
@@ -3669,7 +3669,7 @@
  * Add G-codes M810-M819 to define and run G-code macros.
  * Macros are not saved to EEPROM.
  */
-//#define GCODE_MACROS
+#define GCODE_MACROS
 #if ENABLED(GCODE_MACROS)
   #define GCODE_MACROS_SLOTS       5  // Up to 10 may be used
   #define GCODE_MACROS_SLOT_SIZE  50  // Maximum length of a single macro
@@ -3681,33 +3681,59 @@
  */
 
 // Custom Menu: Main Menu
-//#define CUSTOM_MENU_MAIN
+#define CUSTOM_MENU_MAIN
 #if ENABLED(CUSTOM_MENU_MAIN)
-  //#define CUSTOM_MENU_MAIN_TITLE "Custom Commands"
-  #define CUSTOM_MENU_MAIN_SCRIPT_DONE "M117 User Script Done"
-  #define CUSTOM_MENU_MAIN_SCRIPT_AUDIBLE_FEEDBACK
-  //#define CUSTOM_MENU_MAIN_SCRIPT_RETURN   // Return to status screen after a script
-  #define CUSTOM_MENU_MAIN_ONLY_IDLE         // Only show custom menu when the machine is idle
+  #define CUSTOM_MENU_MAIN_TITLE "Custom Commands"
+  #define USER_SCRIPT_DONE "M117 User Script Done"
+  #define USER_SCRIPT_AUDIBLE_FEEDBACK
+  // #define USER_SCRIPT_RETURN  // Return to status screen after a script
+																							  
 
   #define MAIN_MENU_ITEM_1_DESC "Home & UBL Info"
   #define MAIN_MENU_ITEM_1_GCODE "G28\nG29 W"
-  //#define MAIN_MENU_ITEM_1_CONFIRM          // Show a confirmation dialog before this action
+																							  
 
   #define MAIN_MENU_ITEM_2_DESC "Preheat for " PREHEAT_1_LABEL
   #define MAIN_MENU_ITEM_2_GCODE "M140 S" STRINGIFY(PREHEAT_1_TEMP_BED) "\nM104 S" STRINGIFY(PREHEAT_1_TEMP_HOTEND)
-  //#define MAIN_MENU_ITEM_2_CONFIRM
 
-  //#define MAIN_MENU_ITEM_3_DESC "Preheat for " PREHEAT_2_LABEL
-  //#define MAIN_MENU_ITEM_3_GCODE "M140 S" STRINGIFY(PREHEAT_2_TEMP_BED) "\nM104 S" STRINGIFY(PREHEAT_2_TEMP_HOTEND)
-  //#define MAIN_MENU_ITEM_3_CONFIRM
+  #define MAIN_MENU_ITEM_3_DESC "Preheat for " PREHEAT_2_LABEL
+  #define MAIN_MENU_ITEM_3_GCODE "M140 S" STRINGIFY(PREHEAT_2_TEMP_BED) "\nM104 S" STRINGIFY(PREHEAT_2_TEMP_HOTEND)
 
-  //#define MAIN_MENU_ITEM_4_DESC "Heat Bed/Home/Level"
-  //#define MAIN_MENU_ITEM_4_GCODE "M140 S" STRINGIFY(PREHEAT_2_TEMP_BED) "\nG28\nG29"
-  //#define MAIN_MENU_ITEM_4_CONFIRM
+  #define MAIN_MENU_ITEM_4_DESC "Heat Bed/Home/Level"
+  #define MAIN_MENU_ITEM_4_GCODE "M140 S" STRINGIFY(PREHEAT_2_TEMP_BED) "\nG28\nG29"
 
-  //#define MAIN_MENU_ITEM_5_DESC "Home & Info"
-  //#define MAIN_MENU_ITEM_5_GCODE "G28\nM503"
-  //#define MAIN_MENU_ITEM_5_CONFIRM
+  #define MAIN_MENU_ITEM_5_DESC "Home & Info"
+  #define MAIN_MENU_ITEM_5_GCODE "G28\nM503"
+  
+  #define MAIN_MENU_ITEM_6_DESC "Extrude 100mm Filament" 
+  #define MAIN_MENU_ITEM_6_GCODE "M109 S195\nG21\nG90\nG92 E0\nG1 E100 F100\nG92 E0" 
+  
+  #define MAIN_MENU_ITEM_7_DESC "Probe Z height"
+  #define MAIN_MENU_ITEM_7_GCODE "G33 P1"
+  
+  #define MAIN_MENU_ITEM_8_DESC "Default Zprobe 16.20"
+  #define MAIN_MENU_ITEM_8_GCODE "M851 Z-16.20"
+  
+  #define MAIN_MENU_ITEM_9_DESC "Default Zprobe 23.60"
+  #define MAIN_MENU_ITEM_9_GCODE "M851 Z-23.60"
+  
+  #define MAIN_MENU_ITEM_10_DESC "SET CURRENT Z=0"
+  #define MAIN_MENU_ITEM_10_GCODE "G92 Z0"
+
+  //#define MAIN_MENU_ITEM_11_DESC "Disable Soft Endstop"
+  //#define MAIN_MENU_ITEM_11_GCODE "M211 S0"
+
+  //#define MAIN_MENU_ITEM_12_DESC "Enable Soft Endstop"
+  //#define MAIN_MENU_ITEM_12_GCODE "M211 S1"
+									
+
+  #define MAIN_MENU_ITEM_11_DESC "Effector Maintenance"
+  #define MAIN_MENU_ITEM_11_GCODE "G28\n G1 Z410 F3000\n G1 X0 Y-185 F3000\n G1 Z430 F3000 "
+									
+
+											   
+											  
+									
 #endif
 
 // Custom Menu: Configuration Menu
